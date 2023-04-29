99e242379fceee6651706c89d075b6b9faf1bf0d
Emzkie The Great <63085286+hwangxiaoman@users.noreply.github.com>
2023-04-29 10:37:02 +0800

    minor adjustments

--- routes/cipher.js
+++ routes/cipher.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const cipherController = require('../controllers/cipherController');
 
-router.get('/cipher', cipherController.geCipher);
+router.get('/cipher', cipherController.getCipher);
 
 module.exports = router;
